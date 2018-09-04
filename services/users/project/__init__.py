6cfc52bd4f266ed7c65d802f7e8a55c7c52baf8e
Damola Adeagbo <wetals@hotmail.co.uk>
2018-09-04 22:26:59 +0100

    Added React

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -4,10 +4,12 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS
 
 # instantiate the db
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
 
 
 # new
@@ -16,12 +18,16 @@ def create_app(script_info=None):
     # instantiate the app
     app = Flask(__name__)
 
+    # enable CORS
+    CORS(app)
+
     # set config
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
 
     # register blueprints
     from project.api.users import users_blueprint
